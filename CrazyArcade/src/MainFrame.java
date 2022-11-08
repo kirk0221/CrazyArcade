09613cde9f61bba854b18015d60bb9452ea386ad
masub <masub@DESKTOP-LRSJDID>
2022-11-08 10:52:17 +0900

    마수빈

--- CrazyArcade/src/MainFrame.java
+++ CrazyArcade/src/MainFrame.java
@@ -10,11 +10,12 @@ public class MainFrame extends JFrame implements ActionListener {
 	
 	private String MAP_PANGLAND = "PangLand";
 	private String MAP_VILLAGE = "Village";
+	private String MAP_COOKIE = "Cookie";
 	private JButton[] mapButtons; //맵 관리용 멤버필드
 	
 	public MainFrame(){ //MainFrame 생성자
 		this.setTitle("CrazyArcade"); //창 제목
-		this.setSize(800, 600); //창 크기
+		this.setSize(800, 800); //창 크기               // 창 크기 수정
 		this.setResizable(false);//창 크기 고정
 		this.setLayout(new BorderLayout());///BorderLayout 설정
 		
@@ -27,12 +28,15 @@ public class MainFrame extends JFrame implements ActionListener {
 	
 	private JPanel centerPanel() { //창 가운데 들어갈 패널(맵 선택)
 		JPanel centerPanel = new JPanel(); //return할 패널 생성
-		mapButtons = new JButton[2]; //맵 총 개수 지정
+		mapButtons = new JButton[3]; //맵 총 개수 지정
 		JButton PangLand = new JButton(MAP_PANGLAND); //팡랜드(맵0)
 		JButton Village = new JButton(MAP_VILLAGE); //빌리지(맵1)
+		JButton Cookie = new JButton(MAP_COOKIE); //쿠키(맵2)
 		//mapButtons에 각 map 추가
 		mapButtons[0] = PangLand;
 		mapButtons[1] = Village;
+		mapButtons[2] = Cookie;
+		
 		for (int i = 0; i<mapButtons.length; i++) {
 			mapButtons[i].addActionListener(this); //버튼 눌렀을때 맵 선택되도록
 			centerPanel.add(mapButtons[i]); //맵 버튼 패널에 추가
@@ -49,6 +53,9 @@ public class MainFrame extends JFrame implements ActionListener {
 			this.setVisible(false); //현재 창 숨기기
 		}else if (e.getActionCommand().equals(MAP_VILLAGE)) { //빌리지(맵1) 선택시
 			new Village(); //빌리지 창 생성
+			this.setVisible(false); //헌재 창 숨기기
+		}else if (e.getActionCommand().equals(MAP_COOKIE)) { //쿠키(맵2) 선택시
+			new Cookie(); //쿠키 창 생성
 			this.setVisible(false); //헌재 창 숨기기
 		}
 	}
