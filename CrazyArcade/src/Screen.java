7fe0f9c2abdaa428230398b0cba56f042ff89204
JUN <JUN@DESKTOP-1P6V3VM>
2022-11-09 20:38:18 +0900

    코드 수정 업로드

--- CrazyArcade/src/Screen.java
+++ CrazyArcade/src/Screen.java
@@ -6,24 +6,17 @@ import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 import javax.swing.ImageIcon;
-
-
-
-
-/* branch and push 테스트를 위한 수정*/
-
-
-
-
 public class Screen extends Canvas implements KeyListener, ComponentListener {
 	private Image bufferedImage;
 	private Graphics bufferGraphics;
 	private Dimension dim;
 	private Character player1; //플레이어1 생성
 	private Character player2; //플레이어2 생성
+
 	
 	public Screen() {
 		player1 = new Dao(this); //플레이어1에 다오 생성
@@ -40,10 +33,8 @@ public class Screen extends Canvas implements KeyListener, ComponentListener {
 				repaint();
 			}
 		},0, 1);
-		System.out.println("커밋 테스트");
-		System.out.println("커밋 테스트");
-		System.out.println("커밋 테스트");
-		System.out.println("커밋 테스트2");
+		/* 코드 수정 및 업로드 테스트*/
+		System.out.println("업로드 테스트");
 	}
 	
 	public void paint(Graphics g) {//스크린에 그리는 부분
@@ -62,57 +53,58 @@ public class Screen extends Canvas implements KeyListener, ComponentListener {
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		switch(e.getKeyCode()) {//player1에 대한 움직임
-		case KeyEvent.VK_UP:
-			if(player1.getY()>=5) {
-				player1.up();
-			}
-			break;
-		case KeyEvent.VK_DOWN:
-			if(player1.getY()<=520) {
-				player1.down();
-			}
-			break;
-		case KeyEvent.VK_LEFT:
-			if(player1.getX()>=5) {
-				player1.left();
-			}
-			break;
-		case KeyEvent.VK_RIGHT:
-			if(player1.getX()<=740) {
-				player1.right();
-			}
-			break;
+		
+				switch(e.getKeyCode()) {//player1에 대한 움직임
+				case KeyEvent.VK_UP:
+					if(player1.getY()>=5) {
+						player1.up();
+					}
+					break;
+				case KeyEvent.VK_DOWN:
+					if(player1.getY()<=520) {
+						player1.down();
+					}
+					break;
+				case KeyEvent.VK_LEFT:
+					if(player1.getX()>=5) {
+						player1.left();
+					}
+					break;
+				case KeyEvent.VK_RIGHT:
+					if(player1.getX()<=740) {
+						player1.right();
+					}
+					break;
+				}
+				switch(e.getKeyCode()) {//player2에 대한 움직임
+				case KeyEvent.VK_W:
+					if(player2.getY()>=5) {
+						player2.up();
+					}
+					break;
+				case KeyEvent.VK_S:
+					if(player2.getY()<=520) {
+						player2.down();
+					}
+					break;
+				case KeyEvent.VK_A:
+					if(player2.getX()>=5) {
+						player2.left();
+					}
+					break;
+				case KeyEvent.VK_D:
+					if(player2.getX()<=740) {
+						player2.right();
+					}
+					break;
+				}
 		}
-		switch(e.getKeyCode()) {//player2에 대한 움직임
-		case KeyEvent.VK_W:
-			if(player2.getY()>=5) {
-				player2.up();
-			}
-			break;
-		case KeyEvent.VK_S:
-			if(player2.getY()<=520) {
-				player2.down();
-			}
-			break;
-		case KeyEvent.VK_A:
-			if(player2.getX()>=5) {
-				player2.left();
-			}
-			break;
-		case KeyEvent.VK_D:
-			if(player2.getX()<=740) {
-				player2.right();
-			}
-			break;
-		}
-	}
+
 
 	@Override
 	public void keyReleased(KeyEvent e) {
