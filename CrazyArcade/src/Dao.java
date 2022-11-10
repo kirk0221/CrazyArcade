658b843f33d2d30392942baf57d6f53d0b46b182
JUN <JUN@DESKTOP-1P6V3VM>
2022-11-10 09:32:17 +0900

    캐릭터 움직임 적용

--- CrazyArcade/src/Dao.java
+++ CrazyArcade/src/Dao.java
@@ -3,23 +3,27 @@ import javax.swing.ImageIcon;
 
 public class Dao extends Character {
 
-	private Image carImg;
+	private Image daoImg;
 	private int X;
 	private int Y;
 	private int step;
+	private ImageIcon dao1 = new ImageIcon("Resources/dao1.png");
+	private ImageIcon dao2 = new ImageIcon("Resources/dao2.png");
+	private ImageIcon dao3 = new ImageIcon("Resources/dao3.png");
+	private ImageIcon dao4 = new ImageIcon("Resources/dao4.png");
 	
 
 	public Dao(Screen screen) {
 		super(screen);
 		// TODO Auto-generated constructor stub
-		this.carImg = new ImageIcon("Resources/car.png").getImage();
+		this.daoImg = dao2.getImage(); /*기본값은 앞모습*/
 		this.X = 400;//초기 X값
 		this.Y = 300;//초기 Y값
 		this.step = 5;//초기 이동 거리
 	}
 	
 	public Image getImg() {//이미지를 스크린에 주기위한 함수
-		return this.carImg;
+		return this.daoImg;
 	}
 	
 	public int getX() {//X값을 스크린에 주기위한 함수
@@ -32,15 +36,19 @@ public class Dao extends Character {
 	
 	public void up() {//위로 가기
 		Y-=step;
+		this.daoImg = dao3.getImage();
 	}
 	public void down() {//아래로 가기
 		Y+=step;
+		this.daoImg = dao2.getImage();
 	}
 	public void left() {//왼쪽으로 가기
 		X-=step;
+		this.daoImg = dao4.getImage();
 	}
 	public void right() {//오른쪽으로 가기
 		X+=step;
+		this.daoImg = dao1.getImage();
 	}
 
 }
