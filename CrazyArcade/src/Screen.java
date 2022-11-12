c6921330e648acbab5be8ef6b856654898be9479
kirk0221 <kirk0221@naver.com>
2022-11-12 22:02:32 +0900

    players 배열로 관리하게 변경

--- CrazyArcade/src/Screen.java
+++ CrazyArcade/src/Screen.java
@@ -19,10 +19,17 @@ public class Screen extends Canvas implements KeyListener, ComponentListener {
 	public int map_selection;//어떤 맵이 골라졌는지
 	public int[][] map_size;//맵 사이즈 설정을 위한 배열
 	
-	private Character player1; //플레이어1 생성
-	private Character player2; //플레이어2 생성
+	public int[] mapXlocationlist;
+	public int[] mapYlocationlist;
+	public int[][] mapindexlist;
+	
+	private final int MAX_PLAYER = 2;//MAX 플레이어
+	private Character[] players; //플레이어 관리를 위한 배열
 	WaterBalloon player1WaterBalloon; //플레이어1 물풍선 생성
 	WaterBalloon player2WaterBalloon; //플레이어2 물풍선 생성
+	
+	int[] playerIndex_x; /*characterin에서 사용*/
+	int[] playerIndex_y;
 	
 	
 	private Image map_CookieBackground = new ImageIcon("Resources/mapCookie.png").getImage();//쿠키(맵0) 이미지
@@ -30,8 +37,13 @@ public class Screen extends Canvas implements KeyListener, ComponentListener {
 	
 	public Screen(int map) {
 		this.map_selection = map; //생성자를 통해 어떤 맵 설정되었는지 받아오기 위함
-		player1 = new Bazzi(this); //플레이어1에 다오 생성
-		player2 = new Bazzi(this); //플레이어2에 다오 생성
+		players = new Character[MAX_PLAYER];
+		playerIndex_x = new int[MAX_PLAYER];
+		playerIndex_y = new int[MAX_PLAYER];
+		Character player1 = new Bazzi(this); //플레이어1에 배찌 생성
+		Character player2 = new Bazzi(this); //플레이어2에 배찌 생성
+		players[0] = player1;
+		players[1] = player2;
 		addKeyListener(this);
 		addComponentListener(this);
 
@@ -41,6 +53,18 @@ public class Screen extends Canvas implements KeyListener, ComponentListener {
 				this.map_size[i][j] = 0;
 			}
 		}
+		
+		/* mapXlocaionlist와 mapYlocationlist는 맵의 각 타일들의 중심좌표의 x와 y값을 각각 저장*/
+		/*for문을 이용하여 첫 타일은 (10,10)에서 시작해 x와 y 각각 60씩 증가하며 중심좌표들이 저장됨*/
+		this.mapXlocationlist = new int[13];
+		this.mapYlocationlist = new int[13];
+		int locationnum = 10;
+		for(int i=0; i<13;i++) {
+				this.mapXlocationlist[i] = locationnum;
+				this.mapYlocationlist[i] = locationnum;
+				locationnum += 60;
+		}
+		
 		
 		Timer timer = new Timer();
 		timer.schedule(new TimerTask() {//0.001초 주기로 repaint
@@ -62,16 +86,28 @@ public class Screen extends Canvas implements KeyListener, ComponentListener {
 		}else if(map_selection == 1) {//해적맵일때 배경
 			bufferGraphics.drawImage(map_PatriotsBackground,0,0,this);
 		}
-		bufferGraphics.drawImage(player1.getImg(), player1.getX(), player1.getY(), this);//player1 이미지 생성
-		bufferGraphics.drawImage(player2.getImg(), player2.getX(), player2.getY(), this);//player2 이미지 생성
+		for(int i=0; i<MAX_PLAYER; i++) {
+		bufferGraphics.drawImage(players[i].getImg(), players[i].getX(), players[i].getY(), this);//players 이미지 생성
+		}
 		g.drawImage(this.bufferedImage, 0, 0, this);
-		this.characterin();
 	}
 	
 	public void characterin() {//캐릭터가 현재 맵의 어느 배열위치에 있는지 확인
-		int p1_x=player1.getX()/50;//이거 나눠서 나오는 값이랑 그림에서 나오게 되는 좌표랑 같게 연동시켜줘야함(김준씨 부탁해요)
-		int p1_y=player1.getY()/50;//이값도 바꿔야됨
-		System.out.println("p1의 위치는 : ("+player1.getX()+","+player1.getY()+")"+"p1의 인덱스 위치는 : ("+p1_x+","+p1_y+")");
+		/* mapXlocaitonlist의 13개의 중심 좌표값을 현재의 플레이어 X좌표와 비교하여 그 차이가 40보다 작으면 인덱스를 해당 중심좌표의 인덱스로 변경함*/
+		for (int j=0; j<MAX_PLAYER; j++) {
+			for(int i=0; i<13;i++) {
+				if((-(players[i].getX()-mapXlocationlist[i])<40) && ((mapXlocationlist[i]-players[i].getX())<40)) {
+					playerIndex_x[j] = i;
+				}
+			}
+			/* mapYlocaitonlist의 13개의 중심 좌표값을 현재의 플레이어 Y좌표와 비교하여 그 차이가 40보다 작으면 인덱스를 해당 중심좌표의 인덱스로 변경함*/
+			for(int i=0; i<13;i++) {
+				if((-(players[i].getY()-mapYlocationlist[i])<40) && ((mapYlocationlist[i]-players[i].getY())<40)) {
+					playerIndex_y[j] = i;
+				}
+			}
+			System.out.println("p"+j+1+"의 위치는 : ("+players[j].getX()+","+players[j].getY()+")"+"p1의 인덱스 위치는 : ("+playerIndex_x[j]+","+playerIndex_y[j]+")");
+		}
 	}
 	
 	public void update(Graphics g) {//업데이트 함수
@@ -89,54 +125,54 @@ public class Screen extends Canvas implements KeyListener, ComponentListener {
 		// TODO Auto-generated method stub
 		switch(e.getKeyCode()) {//player1에 대한 움직임
 		case KeyEvent.VK_UP:
-			if(player1.getY()>=0) {
-				player1.up();
+			if(players[0].getY()>=0) {
+				players[0].up();
 			}
 			break;
 		case KeyEvent.VK_DOWN:
-			if(player1.getY()<=800) {// 창현아 여기 숫자 바꿔서 안나가게 딱 막아줘
-				player1.down();
+			if(players[0].getY()<=800) {// 창현아 여기 숫자 바꿔서 안나가게 딱 막아줘
+				players[0].down();
 			}
 			break;
 		case KeyEvent.VK_LEFT:
-			if(player1.getX()>=0) {
-				player1.left();
+			if(players[0].getX()>=0) {
+				players[0].left();
 			}
 			break;
 		case KeyEvent.VK_RIGHT:
-			if(player1.getX()<=800) {// 창현아 여기 숫자 바꿔서 안나가게 딱 막아줘
-				player1.right();
+			if(players[0].getX()<=800) {// 창현아 여기 숫자 바꿔서 안나가게 딱 막아줘
+				players[0].right();
 			}
 			break;
 		case KeyEvent.VK_SHIFT:
 			player1WaterBalloon = new WaterBalloon();
-			player1WaterBalloon.makeWaterBalloon(player1.getX(), player1.getY());//물풍선 놓기
+			player1WaterBalloon.makeWaterBalloon(players[0].getX(), players[0].getY());//물풍선 놓기
 			break;
 		}
 		switch(e.getKeyCode()) {//player2에 대한 움직임
 		case KeyEvent.VK_W:
-			if(player2.getY()>=0) {
-				player2.up();
+			if(players[1].getY()>=0) {
+				players[1].up();
 			}
 			break;
 		case KeyEvent.VK_S:
-			if(player2.getY()<=800) {// 창현아 여기 숫자 바꿔서 안나가게 딱 막아줘
-				player2.down();
+			if(players[1].getY()<=800) {// 창현아 여기 숫자 바꿔서 안나가게 딱 막아줘
+				players[1].down();
 			}
 			break;
 		case KeyEvent.VK_A:
-			if(player2.getX()>=0) {
-				player2.left();
+			if(players[1].getX()>=0) {
+				players[1].left();
 			}
 			break;
 		case KeyEvent.VK_D:
-			if(player2.getX()<=800) {// 창현아 여기 숫자 바꿔서 안나가게 딱 막아줘
-				player2.right();
+			if(players[1].getX()<=800) {// 창현아 여기 숫자 바꿔서 안나가게 딱 막아줘
+				players[1].right();
 			}
 			break;
 		case KeyEvent.VK_SHIFT:
 			player2WaterBalloon = new WaterBalloon();
-			player2WaterBalloon.makeWaterBalloon(player2.getX(), player2.getY());//물풍선 놓기
+			player2WaterBalloon.makeWaterBalloon(players[1].getX(), players[1].getY());//물풍선 놓기
 			break;
 		}
 	}
