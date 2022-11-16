d2003f687da6d93c4c71c282482a5a24da4f3e5c
임창현 <@desktop>
2022-11-16 17:21:54 +0900

    김준씨꺼에 배찌 이동시 좌측,우측,정면,후면 다보이게 하는거로 바꿈(민건)

---
+++ CrazyArcade/src/MAP_Patriots.java
@@ -0,0 +1,18 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+public class MAP_Patriots extends JFrame{
+	public MAP_Patriots(){ //Patriots 생성자
+		this.setTitle("Patriots"); //창 제목
+		this.setSize(796, 817);//창 크기 -> Screen이 780,780으로 그려짐
+		this.setResizable(false);//창 크기 고정
+		this.setLayout(new BorderLayout());///BorderLayout 설정
+		
+		this.add(new Screen(1));
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //윈도우 창 종료시 프로세스까지 모두 종료
+		this.setLocationRelativeTo(null); //창 가운데 위치
+		this.setVisible(true); //창 보이게 하기
+	}
+}
--- CrazyArcade/src/MainFrame.java
+++ CrazyArcade/src/MainFrame.java
@@ -1,20 +1,22 @@
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class MainFrame extends JFrame implements ActionListener {
 	
-	private String MAP_PANGLAND = "PangLand";
-	private String MAP_VILLAGE = "Village";
+	private String MAP_COOKIE = "Cookie";
+	private String MAP_PATRIOTS = "Patriots";
 	private JButton[] mapButtons; //맵 관리용 멤버필드
 	
 	public MainFrame(){ //MainFrame 생성자
 		this.setTitle("CrazyArcade"); //창 제목
-		this.setSize(800, 700); //창 크기
+		this.setSize(800, 600); //창 크기
 		this.setResizable(false);//창 크기 고정
 		this.setLayout(new BorderLayout());///BorderLayout 설정
 		
@@ -23,17 +25,23 @@ public class MainFrame extends JFrame implements ActionListener {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //윈도우 창 종료시 프로세스까지 모두 종료
 		this.setLocationRelativeTo(null); //창 가운데 위치
 		this.setVisible(true); //창 보이게 하기
+		
 	}
 	
 	
 	private JPanel centerPanel() { //창 가운데 들어갈 패널(맵 선택)
 		JPanel centerPanel = new JPanel(); //return할 패널 생성
+		
 		mapButtons = new JButton[2]; //맵 총 개수 지정
-		JButton PangLand = new JButton(MAP_PANGLAND); //팡랜드(맵0)
-		JButton Village = new JButton(MAP_VILLAGE); //빌리지(맵1)
+		JButton Cookie = new JButton(MAP_COOKIE); //쿠키(맵0)
+		JButton Patriots = new JButton(MAP_PATRIOTS); //해적(맵1)
+		
+		Cookie.setPreferredSize(new Dimension(100,100)); //팡랜드 버튼 크기 100,100 설정
+		Patriots.setPreferredSize(new Dimension(100,100)); //빌리지 버튼 크기 100,100 설정
+		
 		//mapButtons에 각 map 추가
-		mapButtons[0] = PangLand;
-		mapButtons[1] = Village;
+		mapButtons[0] = Cookie;
+		mapButtons[1] = Patriots;
 		for (int i = 0; i<mapButtons.length; i++) {
 			mapButtons[i].addActionListener(this); //버튼 눌렀을때 맵 선택되도록
 			centerPanel.add(mapButtons[i]); //맵 버튼 패널에 추가
@@ -41,15 +49,14 @@ public class MainFrame extends JFrame implements ActionListener {
 		return centerPanel;
 	}
 
-
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if (e.getActionCommand().equals(MAP_PANGLAND)) { //팡랜드(맵0) 선택시
-			new PangLand(); //팡랜드 창 생성
+		if (e.getActionCommand().equals(MAP_COOKIE)) { //쿠키(맵0) 선택시
+			new MAP_Cookie(); //쿠키(맵0) 창 생성
 			this.setVisible(false); //현재 창 숨기기
-		}else if (e.getActionCommand().equals(MAP_VILLAGE)) { //빌리지(맵1) 선택시
-			new Village(); //빌리지 창 생성
+		}else if (e.getActionCommand().equals(MAP_PATRIOTS)) { //해적(맵1) 선택시
+			new MAP_Patriots(); //해적(맵1) 창 생성
 			this.setVisible(false); //헌재 창 숨기기
 		}
 	}
